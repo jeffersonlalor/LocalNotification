b5c054c46278083cdd3714cd745105d245d9b56f
Jefferson Lalor <jeffersonlalor19@gmail.com>
2020-05-21 10:35:36 -0400

    Local notification working

--- LocalNotification/LocalNotification/ViewController.swift
+++ LocalNotification/LocalNotification/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setReminderTapped(_ sender: Any) {
+        let center = UNUserNotificationCenter.current()
+        let content = UNMutableNotificationContent()
+        
+        content.title = "\(Bundle.main.infoDictionary?["CFBundleName"] ?? "NameFailed")"
+        content.body = "This is a local notification"
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        
+        center.add(request) { (_error) in
+            guard let error = _error else {return}
+            print("Error -> \(error.localizedDescription)")
+        }
+    }
 }
 
